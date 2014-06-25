f8f6485d579f8f6f8098f020598be6fe92a7b4ae
Tim Allen <tim@noblesamurai.com>
2014-06-25 15:57:18 +1000

    Additional test to see that the normal case works.

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,21 @@
 var majestic = require('../index'),
     expect = require('expect.js');
 
-describe('Majestic', function(){
+describe('Majestic', function() {
   describe('#getIndexItemInfo()', function(){
     it('should error on failure', function(done){
       majestic.getIndexItemInfo('key', ['http://google.com', 'http://wikipedia.com'], callback);
       function callback(err) {
         expect(err).to.be.ok();
         expect(err).to.be.an(Error);
+        done();
+      }
+    });
+    it('should return results', function(done){
+      majestic.getIndexItemInfo(process.env.MAJESTIC_API_KEY, ['http://google.com', 'http://wikipedia.com'], callback);
+      function callback(err, result) {
+        expect(err).to.be(null);
+        expect(result).to.be.ok();
         done();
       }
     });
